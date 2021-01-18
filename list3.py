7370b3aff4791c1bcadd28ba34d6322f299fb540
aashish kumar <aashishcs024@gmail.com>
2021-01-19 02:59:12 +0530

    jenkin pthon project

---
+++ list3.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 14 22:28:59 2021
+
+@author: aashish kumar
+"""
+
+n=[0,1,2,3,4,5,6,7,8,9,10]
+i=0
+while i < len(n):
+    print(n[i])
+    i=i+1
+
+for i in n:
+    print(n[i])
